75edba04ea830b1f995caeea3b596991911c7e12
soheeeep <tpfj@naver.com>
2022-09-29 19:43:46 +0900

    update towncrier shell

--- towncrier_shell.py
+++ towncrier_shell.py
@@ -1,28 +1,55 @@
-from operator import le
 import os
+import sys
+import argparse
 import tomli as tomllib
+from pathlib import Path
 
-pr_n_title, pr_labels = "", []
 
-directory = os.path.abspath("./")
-pyproject_toml = os.path.join(directory, "pyproject.toml")
+def get_config():
+    directory = os.path.abspath("./")
+    config_path = Path(os.path.join(directory, "pyproject.toml"))
 
-with open(pyproject_toml, "rb") as conf:
-    bconfig = tomllib.load(conf)
+    with open(config_path, "rb") as conf:
+        config_toml = tomllib.load(conf)
+    config = config_toml["tool"]["towncrier"]
+    return config
 
-config = bconfig["tool"]["towncrier"]
-fragment_types = [t["directory"] for t in config.get("type")]
-fragment_dir = config.get("directory")
-try:
-    files = os.listdir(fragment_dir)    # ['5.feature.md', 'template.md', '5.doc.md']
-except FileNotFoundError as e:
-    raise Exception()
 
-for basename in files:
-    parts = basename.split(".")
-    if len(parts) != 3 or parts[1] not in pr_labels:
-        continue
-    # Update news fragment file content if there is matching label.
-    with open(parts, "rb") as f:
-        ## update file content...
-        pass
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--content', default='',
+        help='Enter the contents for the news fragment to modify.'
+    )
+    parser.add_argument(
+        '--fragments', nargs='+', type=str,
+        help='Enter the list of fragment types for the new fragment you want to modify.'
+    )
+
+    args = parser.parse_args()
+    if not args.content:
+        print("::error ::No contents given from args.")
+        sys.exit(1)
+    if not args.fragments:
+        print("::error ::No fragment types given from args.")
+        sys.exit(1)
+
+    config = get_config()
+    fragment_dir = config.get("directory")
+    try:
+        files = os.listdir(fragment_dir)
+    except FileNotFoundError as e:
+        raise Exception()
+    for basename in files:
+        parts = basename.split(".")
+        if len(parts) != 3 or parts[1] not in args.fragments:
+            continue
+        output_path = Path(os.path.join(fragment_dir, basename))
+        output_path.write_text(args.content)
+        print(
+            f'\n### {parts[1].title()}\n * {args.content} ({basename})', file=sys.stderr)
+    print('Successfully updated the news fragment found by towncrier.', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
