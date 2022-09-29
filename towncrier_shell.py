e30786f70a972fad9a2939a68f91f717ea5a3737
soheeeep <tpfj@naver.com>
2022-09-29 22:45:27 +0900

    add arg_as_list type checking mtd in towncrier sh

--- towncrier_shell.py
+++ towncrier_shell.py
@@ -1,9 +1,17 @@
 import os
+import re
 import sys
 import argparse
 import tomli as tomllib
 from pathlib import Path
+import ast
 
+
+def arg_as_list(s):
+    v = ast.literal_eval(s)
+    if type(v) is not list:
+        raise argparse.ArgumentTypeError('Argument (%s) is not a list.', (s))
+    return v
 
 def get_config():
     directory = os.path.abspath("./")
@@ -22,7 +30,7 @@ def main():
         help='Enter the contents for the news fragment to modify.'
     )
     parser.add_argument(
-        '--fragments', nargs='+', type=str,
+        '--fragments', type=arg_as_list, default=[],
         help='Enter the list of fragment types for the new fragment you want to modify.'
     )
 
@@ -46,9 +54,8 @@ def main():
             continue
         output_path = Path(os.path.join(fragment_dir, basename))
         output_path.write_text(args.content)
-        print(
-            f'\n### {parts[1].title()}\n * {args.content} ({basename})', file=sys.stderr)
-    print('Successfully updated the news fragment found by towncrier.', file=sys.stderr)
+        print(f'\n### {parts[1].title()}\n * {args.content} ({basename})', file=sys.stderr)
+        print(f'Successfully updated the "{parts[1].title()}" news fragment found by towncrier.\n', file=sys.stderr)
 
 
 if __name__ == '__main__':
